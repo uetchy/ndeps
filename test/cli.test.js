4e3f6fe6ca8ced90bdb4d85293e02b82a2612ccb
Yasuaki Uechi <y@uechi.io>
2019-01-18 19:38:56 +0900

    add test

---
+++ test/cli.test.js
@@ -0,0 +1,14 @@
+const assert = require('assert')
+const path = require('path')
+const { exec } = require('child_process')
+
+const bin = path.join(__dirname, '..', require('../package.json').bin.ndl)
+
+describe('cli', () => {
+  it('list deps', done => {
+    exec(`node ${bin}`, (err, stdout, stderr) => {
+      assert.ok(stdout.includes('chalk'))
+      done()
+    })
+  })
+})
